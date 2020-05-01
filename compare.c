8cb618ec3daefdddacaa0bb8d4cdf82e13a53c10
prodigy-24 <nitingowdru24@gmail.com>
2020-05-01 15:23:58 +0530

    addded compare.c

---
+++ compare.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int j, k;
+    scanf("%d %d", &j, &k);
+
+    if (j == k)
+    {
+        printf ("same");
+    }
+    else
+    {
+       printf ("different");      
+    }
+}
