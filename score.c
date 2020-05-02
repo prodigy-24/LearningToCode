95a67198e87bc10c60e4237898e1c0c240cf21ba
prodigy-24 <nitingowdru24@gmail.com>
2020-05-02 14:05:07 +0530

    added score.c

---
+++ score.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int score1,score2,score3;
+    scanf("%d %d %d",&score1,&score2,&score3);
+    
+    printf("score1:");
+    for(int i = 0 ; i <score1; i++)
+    {
+        printf("#");
+        
+    }
+    printf("\n");
+}
